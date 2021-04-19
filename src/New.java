155d5f125152ab3563cda926da8f6b45b2782192
hasiburrahman123 <hasibur@2018.cqut.edu.cn>
2021-04-20 00:22:24 +0800

    create to new class

---
+++ src/New.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName: New
+ * @Description: TODO
+ * @Author: 哈西
+ * @Date: 2021/4/20 0:21
+ * @Version: v1.0
+ */
+public class New {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
